0db3bf65f9221573acebd184f32c3fbf14d048ba
Santiago Arocha <arochas846@gmail.com>
2022-12-23 22:50:36 -0300

    Descitption modified

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ function App() {
   return (
      <div className="App">
        <div className='principal_container'>
-          <h1 className='title'>Naruto Characters</h1>
-          <h2 className='subtitle'>These are our best shinobies testimonies: What do you think about them?</h2>
+          <h1 className='title'>Anime Characters</h1>
+          <h2 className='subtitle'>These are our best warriors descriptions: What do you think about them?</h2>
           <Testimony 
           name='Boruto'
           country='Leaf'
           image='boruto'
           job='Shinobi'
           company='7 Team'
-          description="I'm a genious, I was considered as one my whole life"        
+          description='Boruto Uzumaki(うずまきボルト, Uzumaki Boruto) is a shinobi from Konoha, from Uzumaki clan and a direct descendant of the Hyūga clan through his mother. While initially resentful of his father and his absence since becoming Hokage, Boruto eventually comes to respect his father and duties. Regardless, he vows to instead become a shinobi like his mentor.' 
           />
 
           <Testimony 
@@ -26,7 +26,7 @@ function App() {
           image='flash'
           job='Samurai'
           company='Deamon Slayer'
-          description="I am considered as a normal person, but I have a secret power"        
+          description="Master Swordsman: Zenitsu is an incredibly talented swordsman, especially in his proficiency in extremely fast Iaijutsu-like swordsmanship and the Thunderclap and Flash technique, to the point he can create offshoots of the technique and utilize it even while he is asleep."         
           />
 
           <Testimony 
@@ -35,7 +35,7 @@ function App() {
           image='Kakashi'
           job='Shinobi'
           company='7 Team'
-          description="I was supposed to be big, and I became the Hokage"        
+          description="Kakashi Hatake (はたけカカシ, Hatake Kakashi) is a shinobi of Konohagakure's Hatake clan. Famed as Kakashi of the Sharingan (写輪眼のカカシ, Sharingan no Kakashi), he is one of Konoha's most talented ninja, regularly looked to for advice and leadership despite his personal dislike of responsibility. To his students on Team 7, Kakashi emphasises the importance of teamwork; he himself received this lesson, along with the Sharingan, from his childhood friend, Obito Uchiha. After the Fourth Shinobi World War, Kakashi becomes Konoha's Sixth Hokage (六代目火影, Rokudaime Hokage, literally meaning: Sixth Fire Shadow)."         
           />
 
           <Testimony 
@@ -44,7 +44,7 @@ function App() {
           image='minato'
           job='Shinobi'
           company='7 Team'
-          description="I was the most incredible shinobi ever, and I defeated Obito"        
+          description="Minato Namikaze (波風ミナト, Namikaze Minato) was the Fourth Hokage (四代目火影, Yondaime Hokage, literally meaning: Fourth Fire Shadow) of Konohagakure. He was renowned all over the world as Konoha's Yellow Flash (木ノ葉の黄色い閃光, Konoha no Kiiroi Senkō, English TV: Yellow Flash of the Leaf). He died during the Nine-Tailed Demon Fox's Attack, sacrificing his life to seal a part of the Nine-Tails into his newborn son, Naruto Uzumaki."         
           />
 
           <Testimony 
@@ -53,7 +53,7 @@ function App() {
           image='sasuke'
           job='Shinobi'
           company='7 Team'
-          description="I am the only one who can follow him"        
+          description="Sasuke Uchiha (うちはサスケ, Uchiha Sasuke) is one of the last surviving members of Konohagakure's Uchiha clan. After his older brother, Itachi, slaughtered their clan, Sasuke made it his mission in life to avenge them by killing Itachi. He is added to Team 7 upon becoming a ninja and, through competition with his rival and best friend, Naruto Uzumaki, Sasuke starts developing his skills. Dissatisfied with his progress, he defects from Konoha so that he can acquire the strength needed to exact his revenge."         
           />
        </div>
      </div>
