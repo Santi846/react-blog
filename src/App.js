491dff95aba5fec730130f8a3d2380eeb077921a
Santiago Arocha <arochas846@gmail.com>
2023-02-18 12:30:58 -0300

    Class component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,41 @@
 import './App.css';
 import { Testimony } from './components/testimony.jsx';
 import { boruto_object, zenitsu_object, kakashi_object, minato_object, sasuke_object } from './components/props.js';
+import React from 'react';
 
-
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className='principal_container'>
+//            <h1 className='title'>Anime Characters</h1>
+//            <h2 className='subtitle'>These are our best warriors descriptions: What do you think about them?</h2>
+ 
+//            <Testimony 
+//            {...boruto_object}  
+//            />
+ 
+//            <Testimony 
+//            {...zenitsu_object}         
+//            />
+ 
+//            <Testimony 
+//            {...kakashi_object}          
+//            />
+ 
+//            <Testimony 
+//            {...minato_object}        
+//            />
+ 
+//            <Testimony 
+//            {...sasuke_object}      
+//            />
+//         </div>
+//       </div>
+     
+//    );
+//   }
+// }
 
 function App() {
   
--- src/components/testimony.jsx
+++ src/components/testimony.jsx
@@ -1,39 +1,43 @@
 import React from 'react';
 import '../styles/components-styles/testimonials.css';
 
-export function Testimony(props) {
-  if (props.professional_name) {
-  return (
-    <div className="testimony_container">
-      <img className="testimony_image" src={require(`../images/${props.image}.jpg`)} alt={require(`../images/${props.image}.jpg`)} />
-
-      <div className="testimony_text_container">
-        <p className="testimony_name">
-          <strong>{props.name}</strong> of
-          <b> {props.country}</b></p>
-        <p className="testimony_job">
-          <strong>{props.job}</strong> at 
-          <b> {props.company}</b></p>
-          <p className="testimony_text_description"><strong>{props.name}</strong>{props.description}<strong>{props.professional_name}</strong></p>
-          
+export class Testimony extends React.Component{
+  render(){
+    if (this.props.professional_name) {
+      return (
+        <div className="testimony_container">
+          <img className="testimony_image" src={require(`../images/${this.props.image}.jpg`)} alt={require(`../images/${this.props.image}.jpg`)} />
+    
+          <div className="testimony_text_container">
+            <p className="testimony_name">
+              <strong>{this.props.name}</strong> of
+              <b> {this.props.country}</b></p>
+            <p className="testimony_job">
+              <strong>{this.props.job}</strong> at 
+              <b> {this.props.company}</b></p>
+              <p className="testimony_text_description"><strong>{this.props.name}</strong>{this.props.description}<strong>{this.props.professional_name}</strong></p>
+              
+          </div>
+        </div>
+      );
+     }
+     return (
+      <div className="testimony_container">
+        <img className="testimony_image" src={require(`../images/${this.props.image}.jpg`)} alt={require(`../images/${this.props.image}.jpg`)} />
+    
+        <div className="testimony_text_container">
+          <p className="testimony_name">
+            <strong>{this.props.name}</strong> of
+            <b> {this.props.country}</b></p>
+          <p className="testimony_job">
+            <strong>{this.props.job}</strong> at 
+            <b> {this.props.company}</b></p>
+            <p className="testimony_text_description"><strong>{this.props.name}</strong>{this.props.description}</p>
+            
+        </div>
       </div>
-    </div>
-  );
- }
- return (
-  <div className="testimony_container">
-    <img className="testimony_image" src={require(`../images/${props.image}.jpg`)} alt={require(`../images/${props.image}.jpg`)} />
-
-    <div className="testimony_text_container">
-      <p className="testimony_name">
-        <strong>{props.name}</strong> of
-        <b> {props.country}</b></p>
-      <p className="testimony_job">
-        <strong>{props.job}</strong> at 
-        <b> {props.company}</b></p>
-        <p className="testimony_text_description"><strong>{props.name}</strong>{props.description}</p>
-        
-    </div>
-  </div>
-);
+    );
+  }
 }
+
+
